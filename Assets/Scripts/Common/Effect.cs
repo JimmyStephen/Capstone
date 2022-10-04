b496d7137939297309cab91da374d4219ee269d3
JimmyStephen <jstephen@student.neumont.edu>
2022-10-04 14:52:57 -0600

    Day 2
    Added a new model for Artemis, added a effect script that will be used later.
    
    Also imported assets for a map

---
+++ Assets/Scripts/Common/Effect.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Effect
+{
+    //How long it lasts
+    private float duration = 1;
+    //How much to damage/heal per second (0 for none)
+    private float healthDamagePerSec = 0;
+    private float healthHealingPerSec = 0;
+    private float energyDamagePerSec = 0;
+    private float energyHealingPerSec = 0;
+    //How much to multiply stats by 1 is default
+    private float damageMultiplier = 1;
+    private float speedMultiplier = 1;
+    //bonus effects
+    private bool stun = false;
+
+    /// <summary>
+    /// What this effect does
+    /// </summary>
+    /// <param name="duration">How long this lasts</param>
+    /// <param name="healthDamagePerSec">The healing per second</param>
+    /// <param name="healthHealingPerSec">The damage per second</param>
+    /// <param name="energyDamagePerSec">Energy gain per second</param>
+    /// <param name="energyHealingPerSec">Energy damage per second</param>
+    /// <param name="damageMultiplier">damage multiplier</param>
+    /// <param name="speedMultiplier">speed multiplier</param>
+    /// <param name="stun">if this effect stuns</param>
+    public Effect(float duration, float healthDamagePerSec, float healthHealingPerSec, float energyDamagePerSec, float energyHealingPerSec, float damageMultiplier, float speedMultiplier, bool stun)
+    {
+        this.duration = duration;
+        this.healthDamagePerSec = healthDamagePerSec;
+        this.healthHealingPerSec = healthHealingPerSec;
+        this.energyDamagePerSec = energyDamagePerSec;
+        this.energyHealingPerSec = energyHealingPerSec;
+        this.damageMultiplier = damageMultiplier;
+        this.speedMultiplier = speedMultiplier;
+        this.stun = stun;
+    }
+
+    /// <summary>
+    /// If this resource requires an update
+    /// </summary>
+    /// <returns></returns>
+    public bool updateNeeded()
+    {
+        return (healthDamagePerSec != 0 && healthHealingPerSec != 0 && energyHealingPerSec != 0 && energyDamagePerSec != 0);
+    }
+
+    /// <summary>
+    /// Call during every update, will effect health/energy as needed
+    /// </summary>
+    /// <param name="health">The health to effect</param>
+    /// <param name="energy">Energy to effect</param>
+    public void updateTrigger(Resource health, Resource energy)
+    {
+        health.Heal(healthHealingPerSec);
+        health.Damage(healthDamagePerSec);
+        energy.Heal(energyHealingPerSec);
+        energy.Damage(energyDamagePerSec);
+    }
+
+    public float getDamageMultipler()
+    {
+        return damageMultiplier;
+    }
+    public float getSpeedMultiplier()
+    {
+        return speedMultiplier;
+    }
+    
+    
+    public bool isStunned()
+    {
+        return stun;
+    }
+}
